49804ffee267fdbd104beab3791b3315f92979dc
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-06-09 23:36:00 +0300

    Add basic functionality

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
+# Copyright 2023 Gleb Tolstenev
+#
+# main.py contain source code for game "Words"
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# An ordinary game in which you need to say words starting with the last letter
+# of the previous word.
+
+word_p1 = str(input("Player 1, enter the word: "))
+word_p2 = str(input("Player 2, enter the word: "))
+if word_p2[0] != word_p1[-1]:
+    print("Player 2, your word must start with last letter of player's 1 word")
+    word_p2 = str(input("Player 2, enter the word: "))
+else:
+    print("It's okay, keep going")
 
